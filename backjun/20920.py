6844cb90e8235e72906e03ea7f6b9687f07c98af
Jung ui heon <ky7662@naver.com>
2024-09-11 17:33:01 +0900

    정렬 문제 : 영어단어암기는 괴로워

---
+++ backjun/20920.py
@@ -0,0 +1,18 @@
+import sys
+from collections import defaultdict
+
+input = sys.stdin.read
+data = input().splitlines()
+
+n, m = map(int, data[0].split())
+
+dic = defaultdict(int)
+
+for i in range(1, n + 1):
+    text = data[i]
+    if len(text) >= m:
+        dic[text] += 1
+
+res = sorted(dic.keys(), key=lambda x: (-dic[x], -len(x), x))
+
+sys.stdout.write("\n".join(res) + "\n")
