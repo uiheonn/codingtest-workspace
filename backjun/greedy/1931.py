74c940931b10d693a9d173092dccb79cdb82a8cd
Jung ui heon <ky7662@naver.com>
2024-11-09 10:37:41 +0900

    greedy : 회의실 배정

--- backjun/greedy/1931.py
+++ backjun/greedy/1931.py
@@ -1,32 +1,22 @@
-# 11
-# 1 4
-# 3 5
-# 0 6
-# 5 7
-# 3 8
-# 5 9
-# 6 10
-# 8 11
-# 8 12
-# 2 13
-# 12 14
-# (1,4)(5,7)(8,11)(12,14)
+import sys
 
-n = int(input())
+input = sys.stdin.read
+data = input().splitlines()
+n = int(data[0])
 lst = []
 for i in range(1, n + 1):
-    startAndEnd = list(map(int, input().split()))
+    startAndEnd = list(map(int, data[i].split()))
     lst.append(startAndEnd)
 
-print(lst)
+leftLst = sorted(lst, key=lambda x:(x[1], x[0]))
 
-real = []
-for i in range(n):
-    semi = []
-    semi.append(lst[i])
-    for j in range(i+1, n):
-        if semi[-1][1] < lst[j][0]:
-            semi.append(lst[j])
-    real.append(semi)
+def isValid(lst, check): # 이미 스케줄링에 넣은 시간과 중복되는지 확인
+    return lst[1] <= check[0]
 
-print(real)
+count = 0
+checkLst = []
+for i in range(len(leftLst)):
+    if not checkLst or isValid(checkLst[-1], leftLst[i]):
+        checkLst.append(leftLst[i])
+
+print(len(checkLst))
