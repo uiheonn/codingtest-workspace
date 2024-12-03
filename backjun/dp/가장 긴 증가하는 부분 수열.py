657a464e59ebd5a9922eaf268c26fc2a4fbabc37
Jung ui heon <ky7662@naver.com>
2024-12-03 11:55:54 +0900

    DP : 11053번 1차 수정

--- backjun/dp/가장 긴 증가하는 부분 수열.py
+++ backjun/dp/가장 긴 증가하는 부분 수열.py
@@ -7,128 +7,54 @@ import sys
 # 3-3. 넣을 배열은 존재하지 않지만 해당 값보다 큰 수가 포함된 배열이 존재한다 -> 존재하는 큰수를 모두 + 해당 값을 slice하여 새로운 배열을 만든다
 # 함수는 해당 배열에 대한 최소값을 리턴한다
 
-n = int(input())
-lst = list(map(int, input().split())) # 10 20 10 30 20 50
+# n = int(input())
+# lst = list(map(int, input().split())) # 10 20 10 30 20 50
 
-# input = sys.stdin.read
-# Data = input().splitlines()
-# n = int(Data[0])
-# lst = list(map(int, Data[1].split()))
-
-# global data
-# data = []
-# res = []
-# res.append(lst[-1])
-# data.append(res)
-# def dp(start):
-#     global data
-#     startNum = lst[start] # 20
-    
-#     right = False
-#     for ele in data: # 새로운 배열을 생성하는지 체크 -> 맞으면 아래 for문은 수행하지 않는다
-#         if ele[0] > startNum:
-#             right = True
-#             break
-#     if right == True: # 기존의 배열에서 생성하는 경우
-#         appendData = []
-#         for ele in data: # data = [[50, 20], [50, 30]] start = 60
-#             if ele[-1] > startNum: # 넣어야 하는 경우
-#                 ele.append(startNum)
-#             elif ele[0] > startNum: # 사이값인 경우
-#                 count = 0
-#                 for i in ele: # ele = [2,1] startNum = 1
-#                     if startNum > i:
-#                         break
-#                     else:
-#                         count+=1
-#                 lsls = ele[0:count+1]
-#                 lsls.append(startNum)
-#                 appendData.append(lsls)
-#         for ele in appendData:
-#             data.append(ele)
-#         # for ele in appendData:
-#         #     secondRight = True
-#         #     for i in data: # data = [[1,2,3], [1,2,3,4]] ele = [1,2]
-#         #         for num in ele:
-#         #             if num not in i:
-#         #                 secondRight = False
-#         #                 break
-#         #         if secondRight == False:
-#         #             break
-#         #     if secondRight == True:
-#         #         data.append(ele)
-#     else: # 새로운 배열을 생성하는 경우
-#         les = []
-#         les.append(startNum)
-#         data.append(les)
-
-# for i in range(n-2,-1,-1):
-#     dp(i)
-
-# maxNum = 0
-# for i in data:
-#     if len(i) > maxNum:
-#         maxNum = len(i)
-# print(data)
-# print(maxNum)
-
+input = sys.stdin.read
+Data = input().splitlines()
+n = int(Data[0])
+lst = list(map(int, Data[1].split()))
 
 global data
-data = []
-res = []
-res.append(lst[-1])
-data.append(res)
+data = set()
+res = tuple([lst[-1]])
+data.add(res)
 def dp(start):
     global data
     startNum = lst[start] # 20
-    
-    right = False
+    appendData = []
+
     for ele in data: # 새로운 배열을 생성하는지 체크 -> 맞으면 아래 for문은 수행하지 않는다
-        if ele[0] > startNum:
-            right = True
-            break
-    if right == True: # 기존의 배열에서 생성하는 경우
-        appendData = []
-        for ele in data: # data = [[50, 20], [50, 30]] start = 60
-            if ele[-1] > startNum: # 넣어야 하는 경우
-                ele.append(startNum)
-            elif ele[0] > startNum: # 사이값인 경우
-                count = 0
-                for i in ele: # ele = [50, 20] -> 슬라이싱 최적화 해야함
-                    if startNum >= i:
-                        break
-                    else:
-                        count+=1
-                lsls = ele[0:count]
-                lsls.append(startNum)
-                appendData.append(lsls)
-        for ele in appendData:
-            if ele not in data:
-                data.append(ele)
-        # for ele in appendData:
-        #     secondRight = True
-        #     for i in data: # data = [[1,2,3], [1,2,3,4]] ele = [1,2]
-        #         for num in ele:
-        #             if num not in i:
-        #                 secondRight = False
-        #                 break
-        #         if secondRight == False:
-        #             break
-        #     if secondRight == True:
-        #         data.append(ele)
-    else: # 새로운 배열을 생성하는 경우
-        les = []
-        les.append(startNum)
-        data.append(les)
+        if not ele:
+            continue
+
+        if ele[-1] > startNum: # 넣어야 하는 경우
+            new_list = list(ele)
+            new_list.append(startNum)
+            appendData.append(tuple(new_list))
+        elif ele[0] > startNum: # 사이값인 경우
+            count = 0
+            for i in ele:
+                if startNum >= i:
+                    break
+                else:
+                    count+=1
+            lsls = list(ele[:count])
+            lsls.append(startNum)
+            appendData.append(tuple(lsls))
+    for ele in appendData:
+        data.add(ele)
+                    
+    if not any(ele[0] > startNum for ele in data):
+        data.add(tuple([startNum]))
 
 for i in range(n-2,-1,-1):
     dp(i)
-    #print(data)
 
 maxNum = 0
 for i in data:
     if len(i) > maxNum:
         maxNum = len(i)
 
-#print(data)
+# print(data)
 print(maxNum)
