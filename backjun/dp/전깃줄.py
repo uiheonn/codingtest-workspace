c5df8e738c5f2fe1a919b91faa1c326f563409da
Jung ui heon <ky7662@naver.com>
2025-01-02 12:20:54 +0900

    dp : 2565번 x

--- backjun/dp/전깃줄.py
+++ backjun/dp/전깃줄.py
@@ -1,20 +1,19 @@
-# 10
-# 1 6
-# 2 8
-# 3 2
-# 4 9
-# 5 5
-# 6 10
-# 7 4
-# 8 1
-# 9 7
-# 10 3 -> 6 리턴
+import sys
 
 n = int(input())
 lst = []
 for i in range(n):
     tmp = list(map(int, input().split()))
     lst.append(tmp)
+
+# input = sys.stdin.read
+# data = input().splitlines()
+# n = int(data[0])
+# lst = []
+# for i in range(1, n+1):
+#     lst.append(list(map(int, data[i].split())))
+
+# 선을 지나가는 다른 선이 최대값인 경우 -> DP 수행
 
 def check(elec, wholeElec):
     if elec[0] > wholeElec[0] and elec[1] < wholeElec[1]:
@@ -24,34 +23,34 @@ def check(elec, wholeElec):
     else:
         return False
 
-realCount = 0
+def dp(lst,count): # 최소횟수를 구하는거니까 count 필요
+    ttt = None
+    maxLst = []
+    for ele in lst: # 각 선을 지나는 선을 구해서 리스트화
+        cnt = 0
+        for semiEle in lst:
+            if check(ele, semiEle):
+                cnt+=1
+        maxLst.append(cnt)
+    maxNum = max(maxLst)
+    # print("maxNum : ", maxNum)
 
-
-while True:
-    countLst = []
-    maxNum = -1
-    maxLoc = None
-    for i in range(len(lst)):
-        electronic = lst[i]
-        count = 0
-        for j in range(len(lst)):
-            wholeElec = lst[j]
-            if check(electronic, wholeElec):
-                count+=1
-        print(count)
-        if count >= maxNum:
-            maxNum = count
-            maxLoc = i
-
-    print("max locate : ", maxLoc, "max Number : ", maxNum)
-    
     if maxNum == 0:
-        break
+        return count
+    
+    tttLst = []
+    # print("maxLst : ", maxLst)
+    for i in range(len(lst)): # 지나는 선의 최대값을 가지고 DP 수행
+        if maxLst[i] == maxNum:
+            copyLst = lst.copy()
+            copyLst.pop(i)
+            # print("copyLst : ", copyLst)
+            # print()
+            ttt = dp(copyLst, count+1)
+        if ttt:
+            tttLst.append(ttt)
+    # print("tttLst : ", tttLst)
+    return min(tttLst)
 
-    if maxLoc != None:
-        print("delete electronic : ", lst[maxLoc])
-        lst.pop(maxLoc)
-        realCount+=1
-
-print("lst : ", lst)
-print("real count : ", realCount)
+total = dp(lst,0)
+print(total)
