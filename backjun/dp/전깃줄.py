77627771c15c8fdabfba58034fea6e6b072c50ac
Jung ui heon <ky7662@naver.com>
2025-01-02 12:46:42 +0900

    dp : 2565번 o

--- backjun/dp/전깃줄.py
+++ backjun/dp/전깃줄.py
@@ -1,56 +1,34 @@
 import sys
 
-n = int(input())
-lst = []
-for i in range(n):
-    tmp = list(map(int, input().split()))
-    lst.append(tmp)
+# LIS로 해결해야함
+# 왼쪽 cost 기준으로 정렬
+# 오른쪽 cost를 통해 LIS를 구하고 len(lst) - len(LIS)를 리턴하면 정답
 
-# input = sys.stdin.read
-# data = input().splitlines()
-# n = int(data[0])
+# n = int(input())
 # lst = []
-# for i in range(1, n+1):
-#     lst.append(list(map(int, data[i].split())))
+# for i in range(n):
+#     tmp = list(map(int, input().split()))
+#     lst.append(tmp)
 
-# 선을 지나가는 다른 선이 최대값인 경우 -> DP 수행
 
-def check(elec, wholeElec):
-    if elec[0] > wholeElec[0] and elec[1] < wholeElec[1]:
-        return True
-    elif elec[0] < wholeElec[0] and elec[1] > wholeElec[1]:
-        return True
-    else:
-        return False
+input = sys.stdin.read
+data = input().splitlines()
+n = int(data[0])
+lst = []
+for i in range(1, n+1):
+    lst.append(list(map(int, data[i].split())))
+lst.sort()
 
-def dp(lst,count): # 최소횟수를 구하는거니까 count 필요
-    ttt = None
-    maxLst = []
-    for ele in lst: # 각 선을 지나는 선을 구해서 리스트화
-        cnt = 0
-        for semiEle in lst:
-            if check(ele, semiEle):
-                cnt+=1
-        maxLst.append(cnt)
-    maxNum = max(maxLst)
-    # print("maxNum : ", maxNum)
+cnt_lst = []
+lis_lst = []
+for i in range(n):
+    cnt_lst.append(lst[i][1])
 
-    if maxNum == 0:
-        return count
-    
-    tttLst = []
-    # print("maxLst : ", maxLst)
-    for i in range(len(lst)): # 지나는 선의 최대값을 가지고 DP 수행
-        if maxLst[i] == maxNum:
-            copyLst = lst.copy()
-            copyLst.pop(i)
-            # print("copyLst : ", copyLst)
-            # print()
-            ttt = dp(copyLst, count+1)
-        if ttt:
-            tttLst.append(ttt)
-    # print("tttLst : ", tttLst)
-    return min(tttLst)
+for i in range(n):
+    cnt = 1
+    for j in range(i):
+        if cnt_lst[j] < cnt_lst[i]:
+            cnt = max(lis_lst[j]+1, cnt)
+    lis_lst.append(cnt)
 
-total = dp(lst,0)
-print(total)
+print(n - max(lis_lst))
