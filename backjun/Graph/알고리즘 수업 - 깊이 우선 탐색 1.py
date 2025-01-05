08446b16459c4ce304699f71c54a38967f081021
Jung ui heon <ky7662@naver.com>
2025-01-05 12:45:04 +0900

    DFS : 24479번

--- backjun/Graph/알고리즘 수업 - 깊이 우선 탐색 1.py
+++ backjun/Graph/알고리즘 수업 - 깊이 우선 탐색 1.py
@@ -1,67 +1,46 @@
 import sys
 
-sys.setrecursionlimit(10 ** 9)
+sys.setrecursionlimit(10 ** 5)
+
+n,m,r = map(int, input().split())
+graphLst = [[] for _ in range(n)]
+for i in range(m):
+    a,b = map(int, input().split())
+    graphLst[a-1].append(b-1)
+    graphLst[b-1].append(a-1)
 
 # n,m,r = map(int, input().split())
-# lst = []
+# graphLst = [[] for _ in range(n)]
 # for i in range(m):
 #     a,b = map(int, input().split())
-#     lst.append([a,b])
+#     graphLst[a-1].append(b-1)
+#     graphLst[b-1].append(a-1)
 
-input = sys.stdin.read
-data = input().splitlines()
-n,m,r = map(int, data[0].split())
-lst = []
-for i in range(1,m+1):
-    a,b = map(int, data[i].split())
-    lst.append([a,b])
+for ele in graphLst:
+    ele.sort()
 
-visited = [False] * (n+1) # 정점을 방문했는지 확인하는 배열
-stack = [] # 스택을 통해 dfs 수행
-result = []
+# input = sys.stdin.read
+# data = input().splitlines()
+# n,m,r = map(int, data[0].split())
+# graph = [[0 for _ in range(n)] for _ in range(n)]
+# for i in range(1,m+1):
+#     a,b = map(int, data[i].split())
+#     graph[a-1][b-1] = 1
+#     graph[b-1][a-1] = 1
 
-def makeCostLst(start):
-    costLst = []
-    for ele in lst:
-        if start in ele:
-            #if ele[0] == start and visited[ele[1]] != True and ele[1] not in stack:
-            if ele[0] == start and visited[ele[1]] != True:
-                costLst.append(ele[1])
-            #elif ele[1] == start and visited[ele[0]] != True and ele[0] not in stack:
-            elif ele[1] == start and visited[ele[0]] != True:
-                costLst.append(ele[0])
-    costLst.sort(reverse=True)
+visited = [0] * (n) # 정점을 방문했는지 확인하는 배열
+count = 1
 
-    for ele in costLst:
-        stack.append(ele)
+def dfs(start):
+    global count
 
-def dfs_mat(start):
-    if visited[start] == True:
-        return
+    visited[start] = count
+    count+=1
+    
+    for ele in graphLst[start]:
+        if visited[ele] == 0:
+            dfs(ele)
 
-    makeCostLst(start) # start 정점과 연결되어있는 정점의 집합. 내림차순 정렬하여 스택에 추가
-    visited[start] = True # stack에 이미 방문한 정점은 다시 가지 않도록 True로 업데이트
-    result.append(start)
-    # print("stack : ", stack)
-    # print()
-    # for i in range(len(stack)-1, -1, -1):
-    #     dfs_mat(stack[i])
-    while stack != []:
-        st = stack.pop()
-        dfs_mat(st)
+dfs(r-1)
 
-dfs_mat(r)
-
-resultDict = {}
-for i in range(0,len(result)):
-    resultDict[result[i]] = i+1
-
-total = []
-for i in range(1, n+1):
-    tmp = resultDict.get(i)
-    if tmp:
-        total.append(tmp)
-    else:
-        total.append(0)
-
-sys.stdout.write("\n".join(map(str, total)))
+sys.stdout.write("\n".join(map(str, visited)))
