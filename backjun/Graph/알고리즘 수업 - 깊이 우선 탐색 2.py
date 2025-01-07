f3d6d11e5a92d98a9683232a05dad9ecf6ef0686
Jung ui heon <ky7662@naver.com>
2025-01-07 10:36:24 +0900

    DFS : 24480번

---
+++ backjun/Graph/알고리즘 수업 - 깊이 우선 탐색 2.py
@@ -0,0 +1,37 @@
+import sys
+
+sys.setrecursionlimit(10 ** 5)
+
+n,m,r = map(int, input().split())
+graphLst = [[] for _ in range(n)]
+for i in range(m):
+    a,b = map(int, input().split())
+    graphLst[a-1].append(b-1)
+    graphLst[b-1].append(a-1)
+
+# n,m,r = map(int, input().split())
+# graphLst = [[] for _ in range(n)]
+# for i in range(m):
+#     a,b = map(int, input().split())
+#     graphLst[a-1].append(b-1)
+#     graphLst[b-1].append(a-1)
+
+for ele in graphLst:
+    ele.sort(reverse=True)
+
+visited = [0] * (n) # 정점을 방문했는지 확인하는 배열
+count = 1
+
+def dfs(start):
+    global count
+
+    visited[start] = count
+    count+=1
+    
+    for ele in graphLst[start]:
+        if visited[ele] == 0:
+            dfs(ele)
+
+dfs(r-1)
+
+sys.stdout.write("\n".join(map(str, visited)))
