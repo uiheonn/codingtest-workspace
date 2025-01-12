739dd7890397007c4cd4bf7ac5ec42a4553b81cc
Jung ui heon <ky7662@naver.com>
2025-01-12 11:20:17 +0900

    BFS : 24445번

---
+++ backjun/Graph/알고리즘수업-너비우선탐색2.py
@@ -0,0 +1,44 @@
+import sys
+import collections
+
+sys.setrecursionlimit(10 ** 5)
+
+n,m,r = map(int, input().split())
+graphLst = [[] for _ in range(n)]
+for i in range(m):
+    a,b = map(int, input().split())
+    graphLst[a-1].append(b-1)
+    graphLst[b-1].append(a-1)
+
+# n,m,r = map(int, input().split())
+# graphLst = [[] for _ in range(n)]
+# for i in range(m):
+#     a,b = map(int, input().split())
+#     graphLst[a-1].append(b-1)
+#     graphLst[b-1].append(a-1)
+
+for ele in graphLst:
+    ele.sort(reverse=True)
+
+visited = [0] * (n) # 정점을 방문했는지 확인하는 배열
+count = 1
+queue = collections.deque()
+
+def bfs(start):
+    global count
+
+    visited[start] = count
+    count+=1
+    queue.append(start)
+
+    while len(queue) != 0:
+        u = queue.popleft()
+        for ele in graphLst[u]:
+            if visited[ele] == 0:
+                visited[ele] = count
+                count+=1
+                queue.append(ele)
+
+bfs(r-1)
+
+sys.stdout.write("\n".join(map(str, visited)))
