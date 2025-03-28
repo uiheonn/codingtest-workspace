777e132907c931430e5964f9136c9aacf7540be0
Jung ui heon <ky7662@naver.com>
2025-03-28 11:22:24 +0900

    순열 : 15663번

---
+++ baekjoon/permutation/N과M(9).py
@@ -0,0 +1,15 @@
+import itertools
+import sys
+
+n,m = map(int, input().split())
+lst = list(map(int, input().split()))
+lst.sort()
+temp = itertools.permutations(lst, m)
+
+result = set()
+for ele in temp:
+    result.add(ele)
+result = sorted(result)
+for ele in result:
+    sys.stdout.write(" ".join(map(str, ele)))
+    print()
