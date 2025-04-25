ee7f20df70ff467c060ed72d7a23b79e1711725d
Jung ui heon <ky7662@naver.com>
2025-04-25 14:54:41 +0900

    Chapter 2 : 동적 배열

--- codetree/정수명령처리5.py
+++ codetree/정수명령처리5.py
@@ -1,14 +1,14 @@
-a = int(input())
+N = int(input())
 
-list = []
+num = []
 
-for i in range(0, a, 1):
-    b = input().split()
-    if(b[0] == 'push_back'):
-        list.append(b[1])
-    elif(b[0] == 'pop_back'):
-        list.pop()
-    elif(b[0] == 'get'):
-        print(list[int(b[1])-1])
-    elif(b[0] == 'size'):
-        print(len(list))
+for _ in range(N):
+    line = input().split()
+    if line[0] == "push_back":
+        num.append(int(line[1]))
+    elif line[0] == "pop_back":
+        num.pop()
+    elif line[0] == "size":
+        print(len(num))
+    elif line[0] == "get":
+        print(num[int(line[1])-1])
